854e451401d6081d9db65a043f359d685f2b7293
simonbrown33 <simonbrown33@acf29f80-9215-11dd-960d-7f8e913a182d>
2008-12-18 22:19:20 +0000

    add this footer

--- trunk/inc/footer.php
+++ trunk/inc/footer.php
@@ -1,7 +1,16 @@
 
 		
     </div>
-	<div id="footer">sheetUp &ndash; simonbrown33 _At_ gmail _Dot_ com</div>
+	<div id="footer">
+<!-- AddThis Button BEGIN -->
+<script type="text/javascript">var addthis_pub = "simonbrown33";</script>
+<a href="http://www.addthis.com/bookmark.php" onmouseover="return addthis_open(this, '', '[URL]', '[TITLE]')" onmouseout="addthis_close()" onclick="return addthis_sendto()"><img src="http://s7.addthis.com/static/btn/lg-bookmark-en.gif" width="125" height="16" border="0" alt="" /></a><script type="text/javascript" src="http://s7.addthis.com/js/152/addthis_widget.js"></script>
+<!-- AddThis Button END -->
+
+
+<br /><br />
+
+sheetUp &ndash; simonbrown33 _At_ gmail _Dot_ com</div>
 
 
     <script type="text/javascript">
