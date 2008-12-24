e944198be5fd470810ff7c06392326a2bf32a0e9
simonbrown33 <simonbrown33@acf29f80-9215-11dd-960d-7f8e913a182d>
2008-12-24 14:42:43 +0000

    various bits to website for generating bookmarklet

--- inc/footer.php
+++ inc/footer.php
@@ -2,17 +2,21 @@
 		
     </div>
 	<div id="footer">
-<!-- AddThis Button BEGIN -->
-<script type="text/javascript">var addthis_pub = "simonbrown33";</script>
-<a href="http://www.addthis.com/bookmark.php" onmouseover="return addthis_open(this, '', '[URL]', '[TITLE]')" onmouseout="addthis_close()" onclick="return addthis_sendto()"><img src="http://s7.addthis.com/static/btn/lg-bookmark-en.gif" width="125" height="16" border="0" alt="" /></a><script type="text/javascript" src="http://s7.addthis.com/js/152/addthis_widget.js"></script>
-<!-- AddThis Button END -->
+    <?php if($_SERVER['HTTP_HOST'] != "localhost") { ?>
+    
+    <!-- AddThis Button BEGIN -->
+    <script type="text/javascript">var addthis_pub = "simonbrown33";</script>
+    <a href="http://www.addthis.com/bookmark.php" onmouseover="return addthis_open(this, '', '[URL]', '[TITLE]')" onmouseout="addthis_close()" onclick="return addthis_sendto()"><img src="http://s7.addthis.com/static/btn/lg-bookmark-en.gif" width="125" height="16" border="0" alt="" /></a><script type="text/javascript" src="http://s7.addthis.com/js/152/addthis_widget.js"></script>
+    <!-- AddThis Button END -->
 
+    <?php }?>
 
 <br /><br />
 
 sheetUp &ndash; simonbrown33 _At_ gmail _Dot_ com</div>
 
 
+<?php if($_SERVER['HTTP_HOST'] != "localhost") { ?>
     <script type="text/javascript">
 	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
@@ -20,7 +24,11 @@ sheetUp &ndash; simonbrown33 _At_ gmail _Dot_ com</div>
 	<script type="text/javascript">
 	var pageTracker = _gat._getTracker("UA-5460797-2");
 	pageTracker._trackPageview();
-	</script>   
+	</script>
+<?php }?>
+
+
+
 </body>
 </html>
 
